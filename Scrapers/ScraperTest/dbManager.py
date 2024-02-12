b5ed7133808504af9bf9ea4ab9aff261c985af3d
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-02-12 19:52:20 +0000

    Connected to AWS db
    Connected dbManager.py to aws database, dotenv to hide creds

---
+++ Scrapers/ScraperTest/dbManager.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+import dotenv
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+URI = 'dublinbikes.clw8uqmac8qf.eu-west-1.rds.amazonaws.com'
+PORT = 3306
+USER = 'admin'
+DB = 'dbikes'
+
+# Connect to the db
+connection_string = f"mysql+mysqlconnector://{USER}:{DB_PASSWORD}@{URI}:{PORT}"
+engine = create_engine(connection_string, echo=True)
+
+# Testing
+if engine.connect():
+    print("Connection established successfully.")
+else:
+    print("Failed to establish connection.")
