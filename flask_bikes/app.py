b73764fb344145cf9d250ba94bc53668d72c7935
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-02-26 18:47:55 +0000

    created flask app
    Created a basic flask app with necessary folders for issue #10. I also created the /stations route to show all the stations for #11

---
+++ flask_bikes/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from dbManager import engine
+from sqlalchemy import text
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/stations')
+def stations():
+    try:
+        # Connect to the database using the engine from dbManager
+        with engine.connect() as connection:
+            # Execute a query to fetch data from the table
+            query = text("SELECT * FROM station")
+            result = connection.execute(query)
+            # Fetch all rows
+            rows = result.fetchall()
+
+            # Render the specific page template with the fetched data
+            return render_template('stations.html', rows=rows)
+
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ flask_bikes/dbManager.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv('db.env')
+
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+URI = 'dublinbikes.clw8uqmac8qf.eu-west-1.rds.amazonaws.com'
+PORT = 3306
+USER = 'admin'
+DB = 'dbikes'
+
+# Connect to the db
+connection_string = f"mysql+mysqlconnector://{USER}:{DB_PASSWORD}@{URI}:{PORT}/{DB}"
+engine = create_engine(connection_string, echo=True)
+
+
+# Testing
+try:
+    connection = engine.connect()
+    print("Connection established successfully.")
+
+except Exception as e:
+    print("Failed to establish connection:", e)
