2c7b2f797c165bce1feea8e6864062f965531ae0
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-02-27 18:04:19 +0000

    updated the map for issue #12

--- flask_bikes/app.py
+++ flask_bikes/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from dbManager import engine
 from sqlalchemy import text
 
@@ -12,19 +12,19 @@ def index():
 @app.route('/map')
 def stations():
     try:
-        # Connect to the database using the engine from dbManager
         with engine.connect() as connection:
-            # Execute a query to fetch data from the table
             query = text("SELECT * FROM station")
             result = connection.execute(query)
-            # Fetch all rows
             rows = result.fetchall()
 
-            # Render the specific page template with the fetched data
-            return render_template('map.html', rows=rows)
+            # Convert rows to a list of dictionaries
+            stations = [{"lat": row.position_lat, "lng": row.position_lng, "title": row.name} for row in rows]
 
+            # Pass the stations data to the template
+            return render_template('map.html', stations=stations)
     except Exception as e:
         return f"Error: {e}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flask_bikes/static/js/index.js
+++ flask_bikes/static/js/index.js
@@ -1,23 +1,31 @@
 async function initMap() {
+    const dublin = { lat: 53.3476326, lng: -6.266265 };
+  
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+  
+    const map = new Map(document.getElementById("map"), {
+        zoom: 13,
+        center: dublin,
+        mapId: "DEMO_MAP_ID",
+    });
+  
+    try {
+        const response = await fetch('/map');
+        const data = await response.json();
 
-  const dublin = { lat: 53.3476326, lng: -6.266265 };
+        data.forEach(station => {
+            const pos = { lat: station.lat, lng: station.lng };
 
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  const map = new Map(document.getElementById("map"), {
-    zoom: 13,
-    center: dublin,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  const pos = { lat: 53.345159, lng: -6.267393 }
-
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: pos,
-    title: "station1",
-  });
+            new google.maps.Marker({
+                map: map,
+                position: pos,
+                title: station.title,
+            });
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
 initMap();
