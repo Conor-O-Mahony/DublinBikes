ab52564030248674f7c2e2b0ec7c9a4630548b66
Conor-O-Mahony <conor.omahony@ucdconnect.ie>
2024-03-03 19:40:43 +0000

    Pins scale and are coloured base on bike availability #13 #12

--- flask_bikes/app.py
+++ flask_bikes/app.py
@@ -8,20 +8,40 @@ app = Flask(__name__)
 def index():
     return render_template('base.html')
 
-
 @app.route('/map')
 def stations():
     try:
         with engine.connect() as connection:
-            query = text("SELECT * FROM station")
+        
+            query = text("""SELECT station.number, status, bikes, stands, mechanicalBikes, electricalBikes, electricalInternalBatteryBikes, electricalRemovableBatteryBikes, address, banking, capacity, bonus, name, position_lat, position_lng, lastUpdate
+                             FROM
+                                availability, station
+                             WHERE
+                                timestamp = (SELECT MAX(timestamp) FROM dbikes.availability) AND availability.number = station.number
+                             ORDER BY number;""")
+            
             result = connection.execute(query)
             rows = result.fetchall()
-
+            
             # Convert rows to a list of dictionaries
-            stations = [{"lat": row.position_lat, "lng": row.position_lng, "title": row.name} for row in rows]
+            stations = [{"number": row.number,
+                        "lat": row.position_lat,
+                        "lng": row.position_lng,
+                        "name": row.name,
+                        "address": row.address,
+                        "card": row.banking,
+                        "status": row.status, 
+                        "bikes": row.bikes, 
+                        "stands": row.stands, 
+                        "mechbikes": row.mechanicalBikes, 
+                        "elecbikes": row.electricalBikes, 
+                        "elecinternal": row.electricalInternalBatteryBikes, 
+                        "elecremoveable": row.electricalRemovableBatteryBikes, 
+                        "lastupdate": row.lastUpdate} 
+                        for row in rows]
 
             # Pass the stations data to the template
-            return render_template('map.html', stationsData=stations)
+            return render_template('map.html', data=stations)
     except Exception as e:
         return f"Error: {e}"
 
