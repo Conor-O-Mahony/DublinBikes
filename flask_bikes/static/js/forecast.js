12912855c8b7d1bd9707913381c8e83272da8110
Eneko O'Kelly <eneko.okelly@ucdconnect.ie>
2024-04-17 20:25:24 +0100

    Update forecast.js

--- flask_bikes/static/js/forecast.js
+++ flask_bikes/static/js/forecast.js
@@ -1,6 +1,4 @@
 function openForecast() {
-    // Specify the URL you want to open in the new tab
     const url = "/forecast";
-    // Open the new tab
-    window.open(url, '_blank').focus();
+    window.location.href = url;
 }
